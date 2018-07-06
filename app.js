3242840f003f9245183075f93786ab2ee0e7614d
alexg622 <alex.gm62288@gmail.com>
2018-07-06 16:57:30 -0700

    stuff

--- app.js
+++ app.js
@@ -101,7 +101,7 @@
          });
 
          // we can also pass the token to the browser to make requests from there
-         res.redirect('http://localhost:3000/#' +
+         res.redirect('/' +
            querystring.stringify({
              access_token: access_token,
              refresh_token: refresh_token

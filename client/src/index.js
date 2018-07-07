546a8c482f45b99b58faad7d659fd369749bc9ad
alexg622 <alex.gm62288@gmail.com>
2018-07-06 18:03:15 -0700

    Make better

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-document.addEventListener("DOMContentLoaded", () => {
-  debugger
+
   ReactDOM.render(<App />, document.getElementById('root'));
   registerServiceWorker();
-})
